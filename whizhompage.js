8dd89fbe179e634614ad96d1dce6191b6da7c4cb
gentlewhiz <damolaaya@gmail.com>
2024-09-28 14:19:00 +0100

    added index and style files

---
+++ whizhompage.js
@@ -0,0 +1,10 @@
+// window.onscroll = function() {
+//     let header = document.getElementsByTagName('header');
+    
+//     if (window.scrollY > 20) {
+//       header.classList.add('scrolled');
+//     } else {
+//       header.classList.remove('scrolled');
+//     }
+//   };
+le
